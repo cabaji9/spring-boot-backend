f1cedacf7fefba1f54e68f96abc02b632c90c9fc
Hyun woo son lee <hson@advlatam.com>
2017-07-11 16:21:44 -0500

    Added profile for test

--- src/main/java/com/config/SwaggerConfig.java
+++ src/main/java/com/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.config;
 import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -19,6 +20,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
  */
 @Configuration
 @EnableSwagger2
+@Profile("!test")
 public class SwaggerConfig {
     @Bean
     public Docket dataApi() {
